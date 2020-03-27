802e583ccebee37a440fb566fd3471498721d064
MishaVyuzh <vyuzzzh@yandex.ru>
2020-03-27 22:05:49 +0300

    Updates

---
+++ precache-manifest.36353296e59953c17b77b66c523b9eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac8d28376569c25ebc0859b37926275",
+    "url": "/simple-match-game/index.html"
+  },
+  {
+    "revision": "dd7b170630f35a3c2cc3",
+    "url": "/simple-match-game/static/css/main.d038d717.chunk.css"
+  },
+  {
+    "revision": "1ad534b86e94812b9706",
+    "url": "/simple-match-game/static/js/2.7cc2c9b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-match-game/static/js/2.7cc2c9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7b170630f35a3c2cc3",
+    "url": "/simple-match-game/static/js/main.b221d9dd.chunk.js"
+  },
+  {
+    "revision": "2a01f0c9cc5cf1be0db3",
+    "url": "/simple-match-game/static/js/runtime-main.df993e49.js"
+  }
+]);
